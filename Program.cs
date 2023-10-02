38071eefbfa50c6033e4ee1e3ea9aa0122a9ffdb
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-02 14:58:21 +0100

    Added functionality for plus, minus, multiplication and division operations

--- Program.cs
+++ Program.cs
@@ -5,17 +5,37 @@ class Program
     static void Main(string[] args)
     {   
         #nullable disable
-        Console.WriteLine("Welsome to the calculator!");
+        Console.WriteLine("Welcome to the calculator!");
+        Console.WriteLine("==========================");
+        Console.WriteLine("");
+
+        Console.WriteLine("Please enter the operator: ");
+        double firstNumber;
+        double secondNumber;
+        double result;
+        string message = "";
+        string operation = Console.ReadLine();
         
-        Console.Write("Please type your first number: ");
-        double firstNumber = Double.Parse(Console.ReadLine());
+        if(operation != "+" & operation != "-" & operation != "*" & operation != "/")
+            message = "Please enter either +, -, * or /.";
+        else {
+            Console.Write("Please type your first number: ");
+            firstNumber = Double.Parse(Console.ReadLine());
 
-        Console.Write("Please type your second number: ");
-        double secondNumber = Double.Parse(Console.ReadLine());
+            Console.Write("Please type your second number: ");
+            secondNumber = Double.Parse(Console.ReadLine());
 
-        double result = firstNumber * secondNumber;
-
-        Console.WriteLine("The result of {0} * {1} is {2}", firstNumber, secondNumber, result);
-
+            if(operation == "+")
+                result = firstNumber + secondNumber;
+             else if(operation == "-")
+                result = firstNumber - secondNumber;
+             else if(operation == "*")
+                result = firstNumber * secondNumber;
+             else {
+                result = firstNumber / secondNumber;
+             }
+            message = String.Format("The result of {0} {1} {2} is {3}", firstNumber, operation, secondNumber, result);
+        }        
+        Console.WriteLine(message);
     }
 }
