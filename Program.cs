af497ac2fd07d31b1abe433c0e3cffe886ba38f6
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-02 11:52:09 +0100

    Modified to receive two numbers as input from user and output multiplication

--- Program.cs
+++ Program.cs
@@ -3,7 +3,19 @@
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        #nullable disable
         Console.WriteLine("Welsome to the calculator!");
+        
+        Console.Write("Please type your first number: ");
+        double firstNumber = Double.Parse(Console.ReadLine());
+
+        Console.Write("Please type your second number: ");
+        double secondNumber = Double.Parse(Console.ReadLine());
+
+        double result = firstNumber * secondNumber;
+
+        Console.WriteLine("The result of {0} * {1} is {2}", firstNumber, secondNumber, result);
+
     }
 }
