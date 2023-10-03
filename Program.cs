95fc132b94cedaed34c1705fa1ae14626d4d34aa
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-03 11:26:24 +0100

    Added functionality to do calculations on any number of numbers

--- Program.cs
+++ Program.cs
@@ -5,21 +5,48 @@ class Program
     static void Main(string[] args)
     {   
         #nullable disable
+        Console.WriteLine("");
         Console.WriteLine("Welcome to the calculator!");
         Console.WriteLine("==========================");
         Console.WriteLine("");
 
-        Console.WriteLine("Please enter the operator: ");
-        double firstNumber;
-        double secondNumber;
-        double result;
-        string message = "";
+        Console.Write("Please enter the operator: ");
+        double counter = 0;
+        double sumNumbers = 0;
+        string message;
         string operation = Console.ReadLine();
         
         if(operation != "+" & operation != "-" & operation != "*" & operation != "/")
             message = "Please enter either +, -, * or /.";
         else {
-            Console.Write("Please type your first number: ");
+            if (operation == "*"){
+                sumNumbers = 1;
+            }
+            Console.Write("How many numbers do you want to {0}? ", operation);
+            counter = Double.Parse(Console.ReadLine());
+            for (int i = 1; i <= counter; i++)
+            {                
+                Console.Write("Please enter number {0}? ", i);
+                if(operation == "+"){
+                    sumNumbers += Double.Parse(Console.ReadLine());
+                } else if(operation == "-"){
+                    sumNumbers -= Double.Parse(Console.ReadLine());
+                } else if(operation == "*"){
+                    sumNumbers *= Double.Parse(Console.ReadLine());
+                } else if(operation == "/"){
+                    if(i == 1){
+                        sumNumbers = Double.Parse(Console.ReadLine());
+                    } else {
+                        sumNumbers /= Double.Parse(Console.ReadLine());
+                    }
+                }
+            }
+            Console.WriteLine("The answer is {0}", sumNumbers);
+        }        
+    }
+}
+
+/* Console.Write("Please type your first number: ");
             firstNumber = Double.Parse(Console.ReadLine());
 
             Console.Write("Please type your second number: ");
@@ -35,7 +62,4 @@ class Program
                 result = firstNumber / secondNumber;
              }
             message = String.Format("The result of {0} {1} {2} is {3}", firstNumber, operation, secondNumber, result);
-        }        
-        Console.WriteLine(message);
-    }
-}
+ */
